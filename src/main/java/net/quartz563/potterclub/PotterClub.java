7c456441df2e58ffd880e8915041208321b49aef
Quartz563 <quartz135@gmail.com>
2021-03-07 21:23:57 +0000

    first commit

---
+++ src/main/java/net/quartz563/potterclub/PotterClub.java
@@ -0,0 +1,12 @@
+package net.quartz563.potterclub;
+
+import org.mineacademy.fo.plugin.SimplePlugin;
+
+public class PotterClub extends SimplePlugin {
+
+    @Override
+    protected void onPluginStart() {
+
+    }
+
+}
